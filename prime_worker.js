187d179408af06b950be918d9ae5558ef28ab478
Geovane Fedrecheski <geonnave@gmail.com>
2018-01-29 18:15:02 -0200

    Reduce size of canvas and use isPrime instead of isProbablyPrime

--- prime_worker.js
+++ prime_worker.js
@@ -3,7 +3,7 @@ importScripts("./BigInteger.min.js")
 self.addEventListener('message', function(e) {
   number = bigInt(e.data)
 
-  var is = number.isProbablePrime()
+  var is = number.isPrime()
   console.log(is)
 
   self.postMessage(is)
